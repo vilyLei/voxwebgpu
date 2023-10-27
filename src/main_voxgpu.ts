ba03e3909d11d2de33b6146e30310deea646cff3
vilyLei <vily313@126.com>
2023-10-27 20:15:43 +0800

    fix: add some sys files.

---
+++ src/main_voxgpu.ts
@@ -0,0 +1,26 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////           WEB-GPU               /////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+// import { VertColorTriangle as Demo } from "./voxgpu/sample/VertColorTriangle";
+// import { VertColorCube as Demo } from "./voxgpu/sample/VertColorCube";
+// import { ImgTexturedCube as Demo } from "./voxgpu/sample/ImgTexturedCube";
+// import { TexturedCube as Demo } from "./voxgpu/sample/TexturedCube";
+// import { ImgCubeMap as Demo } from "./voxgpu/sample/ImgCubeMap";
+// import { MultiTexturedCube as Demo } from "./voxgpu/sample/MultiTexturedCube";
+// import { BlendTest as Demo } from "./voxgpu/sample/BlendTest";
+// import { MultiMaterialPass as Demo } from "./voxgpu/sample/MultiMaterialPass";
+
+let demoIns = new Demo();
+function main(): void {
+	console.log("------ demo --- init ------");
+	demoIns.initialize();
+	function mainLoop(now: any): void {
+		demoIns.run();
+		window.requestAnimationFrame(mainLoop);
+	}
+	window.requestAnimationFrame(mainLoop);
+	console.log("------ demo --- running ------");
+}
+main();
---
+++ vue.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const resolve = dir => path.join(__dirname, dir);
+const { ENV = '' } = process.env;
+//console.log("process.env: ",process.env);
+console.log("process.env: ", process.env.npm_lifecycle_script);
+
+let devDstStr = process.env.npm_lifecycle_script;
+if (devDstStr.indexOf("vue-cli-service build") < 0) {
+
+    let i = devDstStr.indexOf("./src");
+    devDstStr = devDstStr.slice(i, -1);
+    // if (process.env.npm_lifecycle_script == "vue-cli-service serve --voxtype=dev") {
+    //     devDstStr = "./src/dev.ts";
+    // }
+    // else if (process.env.npm_lifecycle_script == "vue-cli-service serve --voxtype=devWeb") {
+    //     devDstStr = "./src/devWeb.ts";
+    // }
+}
+let appDstStr = devDstStr;
+
+console.log("devDstStr, appDstStr: ", devDstStr, appDstStr);
+module.exports = {
+    pages: {
+        index: {
+            entry: process.env.NODE_ENV === 'production' ? appDstStr : devDstStr
+        }
+    },
+    filenameHashing: false,
+    // 是否为生产环境构建生成 source map
+    productionSourceMap: false,
+
+    configureWebpack: config => {
+        // TODO: ~entry in ./node_modules/@vue/cli-service/lib/commands/build/entry-lib.js
+        // config['resolve'] = {
+        //   extensions: ['.tsx', '.ts', '.js', '.glsl'],
+        //   alias: {}
+        // };
+        if (true) {
+            config.optimization.minimizer[0].options.terserOptions.compress.warnings = false;
+            config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true;
+            config.optimization.minimizer[0].options.terserOptions.compress.drop_debugger = true;
+            config.optimization.minimizer[0].options.terserOptions.compress.pure_funcs = [
+                'console.log'
+            ];
+        }
+    },
+
+    devServer: {
+        port: 9000,
+        disableHostCheck: true
+        // https:true
+    },
+
+    chainWebpack: config => {
+        config.module
+            .rule('glsl-wgsl')
+            .test(/\.(glsl|wgsl)$/)
+            .use('raw')
+            .loader('raw-loader')
+            .end();
+    }
+};
