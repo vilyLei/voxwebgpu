0c535540812a18f9e1da181606dfe13a62105c18
vilyLei <vily313@126.com>
2023-11-13 00:35:18 +0800

    fix: adjust a few codes.

--- src/voxgpu/utils/CommonUtils.ts
+++ src/voxgpu/utils/CommonUtils.ts
@@ -1,6 +1,8 @@
 function copyFromObjectValueWithKey(src: any, dst: any): void {
 	for(var k in src) {
-		dst[k] = src[k];
+		if(src[k] != undefined) {
+			dst[k] = src[k];
+		}
 	}
 }
 export {copyFromObjectValueWithKey}
