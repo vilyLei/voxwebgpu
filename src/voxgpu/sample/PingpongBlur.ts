e2bf39bdca2e4579da51847463e0d610f951316e
vilyLei <vily313@126.com>
2023-11-13 09:04:23 +0800

    fix: adjust a few codes.

--- src/voxgpu/sample/PingpongBlur.ts
+++ src/voxgpu/sample/PingpongBlur.ts
@@ -10,8 +10,8 @@ import fragWGSL from "../material/shader/wgsl/fixScreenPlaneTex.frag.wgsl";
 import blurHWGSL from "../material/shader/wgsl/blurHTex.frag.wgsl";
 import blurVWGSL from "../material/shader/wgsl/blurVTex.frag.wgsl";
 
-const rttTex0 = { diffuse: { uuid: 'rtt0', rttTexture: {} } };
-const rttTex1 = { diffuse: { uuid: 'rtt1', rttTexture: {} } };
+const rttTex0 = { diffuse: { uuid: "rtt0", rttTexture: {} } };
+const rttTex1 = { diffuse: { uuid: "rtt1", rttTexture: {} } };
 const rtts = [rttTex0, rttTex1];
 const attachment = {
 	texture: rttTex0,
@@ -19,17 +19,15 @@ const attachment = {
 	loadOp: "clear",
 	storeOp: "store"
 };
-const colorAttachments = [ attachment ];
+const colorAttachments = [attachment];
 
 class PassGraph extends WGRPassNodeGraph {
-
 	blurEntity: FixScreenPlaneEntity;
-	constructor() { super(); }
-	runBegin(): void {
-		super.runBegin();
+	constructor() {
+		super();
 	}
-	run(): void {
 
+	run(): void {
 		let pass = this.passes[0];
 
 		const entity = this.blurEntity;
@@ -37,9 +35,9 @@ class PassGraph extends WGRPassNodeGraph {
 
 		for (let i = 0; i < 15; ++i) {
 			pass.colorAttachments[0].clearEnabled = i < 1;
-			attachment.texture = rtts[i%2];
-			ms[i%2].visible = false;
-			ms[(i+1)%2].visible = true;
+			attachment.texture = rtts[i % 2];
+			ms[i % 2].visible = false;
+			ms[(i + 1) % 2].visible = true;
 			pass.render();
 		}
 	}
@@ -58,7 +56,7 @@ export class PingpongBlur {
 	}
 
 	private mGraph = new PassGraph();
-	private uniformValues = [{data: new Float32Array([512, 512, 3.0, 0])}];
+	private uniformValues = [{ data: new Float32Array([512, 512, 3.0, 0]) }];
 	private createMaterial(shadinguuid: string, textures: WGTextureDataDescriptor[], type: number): WGMaterial {
 		let shaderCodeSrc = {
 			vert: { code: vertWGSL, uuid: "vert" },
@@ -67,29 +65,24 @@ export class PingpongBlur {
 		shadinguuid += "-" + type;
 		const material = new WGMaterial({
 			shadinguuid,
-			shaderCodeSrc,
+			shaderCodeSrc
 		});
 		material.uniformValues = this.uniformValues;
 		material.addTextures(textures);
 		return material;
 	}
 	private applyBlurPass(clearColor: ColorDataType, extent = [0.4, 0.3, 0.5, 0.5]): void {
-
 		let rs = this.mRscene;
 
 		const graph = this.mGraph;
 
 		attachment.clearValue = clearColor;
 
-
 		let rPass = rs.renderer.appendRenderPass({ separate: true, colorAttachments });
 
 		const diffuseTex = { diffuse: { url: "static/assets/huluwa.jpg", flipY: true } };
 
-		let materials = [
-			this.createMaterial("shd-01", [rttTex0], 0),
-			this.createMaterial("shd-02", [rttTex1], 1)
-		];
+		let materials = [this.createMaterial("shd-01", [rttTex0], 0), this.createMaterial("shd-02", [rttTex1], 1)];
 
 		let rttEntity = new FixScreenPlaneEntity({ extent: [-1, -1, 2, 2], textures: [diffuseTex] });
 		rPass.addEntity(rttEntity);
@@ -103,7 +96,7 @@ export class PingpongBlur {
 		graph.blurEntity = entity;
 
 		extent = [-0.9, -0.9, 0.6, 0.6];
-		entity = new FixScreenPlaneEntity({ extent, flipY: true, textures: [ diffuseTex ] });
+		entity = new FixScreenPlaneEntity({ extent, flipY: true, textures: [diffuseTex] });
 		rs.addEntity(entity);
 	}
 	private initScene(): void {
