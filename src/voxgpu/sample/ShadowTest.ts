05f4421b8f1b6f15c277899d6bbc4968b3f3002a
vilyLei <vily313@126.com>
2023-12-05 00:16:22 +0800

    fix: add pbr material into the shadow demo.

--- src/voxgpu/sample/ShadowTest.ts
+++ src/voxgpu/sample/ShadowTest.ts
@@ -19,6 +19,9 @@ import { WGRPassColorAttachment } from "../render/pipeline/WGRPassColorAttachmen
 
 import { WGRPassNodeGraph } from "../render/pass/WGRPassNodeGraph";
 import { SpecularEnvBrnTexture } from "../texture/SpecularEnvBrnTexture";
+import Vector3 from "../math/Vector3";
+import Color4 from "../material/Color4";
+import { BasePBRMaterial, LightShaderDataParam } from "../material/BasePBRMaterial";
 class ShadowPassGraph extends WGRPassNodeGraph {
 
 	private entities: Entity3D[] = [];
@@ -187,11 +190,15 @@ export class ShadowTest {
 			rpassparam: { multisampleEnabled: true }
 		});
 		this.initScene();
+		// this.initShadowScene();
 		this.initEvent();
 	}
 
 	private mEntities: Entity3D[] = [];
 	private initScene(): void {
+		this.initTexDisp();
+	}
+	private initShadowScene(): void {
 
 		let rc = this.mRscene;
 
@@ -299,6 +306,43 @@ export class ShadowTest {
 		rc.addEntity(plane);
 	}
 	
+	
+	private initTexDisp(): void {
+		let rc = this.mRscene;
+
+		let position = new Vector3(0, -1, 0);
+		let materials = this.createMaterials(position);
+		let plane = new PlaneEntity({
+			axisType: 1,
+			materials,
+			extent:[-600,-600,1200,1200],
+			transform: { position }
+		});
+		rc.addEntity(plane);
+		/*
+		position = new Vector3(0, 0, 180);
+		let materials = this.createMaterials(position);
+		let sphere = new SphereEntity(
+			{
+				radius: 150.0,
+				materials,
+				transform: { position }
+			}
+		);
+		rc.addEntity(sphere);
+
+		position = new Vector3(0, 0, -180);
+		materials = this.createMaterials(position, [4, 1]);
+		let torus = new TorusEntity({
+			axisType: 1,
+			materials,
+			transform: { position }
+		});
+		rc.addEntity(torus);
+		//*/
+
+	}
+
 	private hdrEnvtex = new SpecularEnvBrnTexture();
 	private createBaseTextures(): WGTextureDataDescriptor[] {
 		const albedoTex = { albedo: { url: `static/assets/pbrtex/rough_plaster_broken_diff_1k.jpg` } };
@@ -311,6 +355,98 @@ export class ShadowTest {
 			armTex
 		] as WGTextureDataDescriptor[];
 		return textures;
+	}
+	
+	private createMaterials(position: Vector3, uvParam?: number[]): BasePBRMaterial[] {
+		let textures0 = this.createBaseTextures();
+		// let textures1 = this.createMaskTextures("plastic");
+		// let textures2 = this.createMaskTextures("wall", 'circleWave_disp.png');
+
+		let material0 = this.createMaterial(position, textures0, ["solid"]);
+		this.applyMaterialPPt(material0);
+
+		// let material1 = this.createMaterial(position, textures1, ["transparent"], 'less-equal', material0.getLightParam());
+		// material1.property.inverseMask = false;
+		// this.applyMaterialPPt(material1);
+
+		// let material2 = this.createMaterial(position, textures2, ["transparent"], 'less-equal', material0.getLightParam());
+		// material2.property.inverseMask = true;
+		// this.applyMaterialPPt(material2);
+		// let list = [material0, material1, material2];
+
+		// let list = [material0, material1];
+		let list = [material0];
+		if (uvParam) {
+			for (let i = 0; i < list.length; ++i) {
+				list[i].property.uvParam.value = uvParam;
+			}
+		}
+		return list;
+	}
+	private applyMaterialPPt(material: BasePBRMaterial): void {
+		let property = material.property;
+		property.ambient.value = [0.0, 0.2, 0.2];
+		property.albedo.value = [0.7, 0.7, 0.3];
+		property.arms.roughness = 0.8;
+		property.armsBase.value = [0, 0, 0];
+		// property.uvParam.value = [2, 2];
+		property.param.scatterIntensity = 32;
+	}
+	private mLightParams: LightShaderDataParam[] = [];
+	private createMaterial(position: Vector3DataType, textures: WGTextureDataDescriptor[], blendModes: string[], depthCompare = 'less', lightParam?: LightShaderDataParam): BasePBRMaterial {
+
+		if (!lightParam) {
+			lightParam = this.createLightData(position);
+		}
+		let pipelineDefParam = {
+			depthWriteEnabled: true,
+			faceCullMode: 'back',
+			blendModes,
+			depthCompare
+		};
+		let material = new BasePBRMaterial({ pipelineDefParam });
+		material.setLightParam(lightParam);
+		material.addTextures(textures);
+		return material;
+	}
+
+	private createLightData(position: Vector3DataType): LightShaderDataParam {
+		let pos = new Vector3().setVector4(position);
+		let pv0 = pos.clone().addBy(new Vector3(0, 200, 0));
+		let pv1 = pos.clone().addBy(new Vector3(200, 0, 0));
+		let pv2 = pos.clone().addBy(new Vector3(0, 0, 200));
+		let pv3 = pos.clone().addBy(new Vector3(-200, 0, 0));
+		let pv4 = pos.clone().addBy(new Vector3(0, 0, -200));
+		let posList = [pv0, pv1, pv2, pv3, pv4];
+
+		let c0 = new Color4(0.1 + Math.random() * 13, 0.1 + Math.random() * 13, 0.0, 0.00002);
+		let c1 = new Color4(0.0, 0.1 + Math.random() * 13, 1.0, 0.00002);
+		let c2 = new Color4(0.0, 0.1 + Math.random() * 13, 0.1 + Math.random() * 13, 0.00002);
+		let c3 = new Color4(0.1 + Math.random() * 13, 1.0, 0.1 + Math.random() * 13, 0.00002);
+		let c4 = new Color4(0.5, 1.0, 0.1 + Math.random() * 13, 0.00002);
+
+		let colorList = [c0, c1, c2, c3, c4];
+
+		let pointLightsTotal = posList.length;
+
+		let j = 0;
+		let lightsData = new Float32Array(4 * pointLightsTotal);
+		let lightColorsData = new Float32Array(4 * pointLightsTotal);
+
+		for (let i = 0; i < lightsData.length;) {
+			const pv = posList[j];
+			pv.w = 0.00002;
+			pv.toArray4(lightsData, i);
+
+			const c = colorList[j];
+			c.toArray4(lightColorsData, i);
+
+			j++;
+			i += 4;
+		}
+		let param = { lights: lightsData, colors: lightColorsData, pointLightsTotal };
+		this.mLightParams.push(param);
+		return param;
 	}
 	private mouseDown = (evt: MouseEvent): void => {};
 	run(): void {
