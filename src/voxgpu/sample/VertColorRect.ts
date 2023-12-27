a95d1166b0d65cee9f8cf3446b5d32c21f817eb7
vilyLei <vily313@126.com>
2023-12-28 07:47:09 +0800

    feat: apply the dithering function.

--- src/voxgpu/sample/VertColorRect.ts
+++ src/voxgpu/sample/VertColorRect.ts
@@ -5,6 +5,24 @@ import { WGRenderer } from "../rscene/WGRenderer";
 import Color4 from "../material/Color4";
 
 const vertWGSL =`
+
+struct VSOut {
+    @builtin(position) Position: vec4<f32>,
+    @location(0) color: vec3<f32>,
+    @location(1) pos: vec4<f32>,
+};
+
+@vertex
+fn main(@location(0) inPos: vec3<f32>,
+        @location(1) inColor: vec3<f32>) -> VSOut {
+    var vsOut: VSOut;
+    vsOut.Position = vec4<f32>(inPos, 1.0);
+    vsOut.color = inColor;
+    vsOut.pos = vsOut.Position;
+    return vsOut;
+}
+`;
+const fragWGSL = `
 const PI = 3.14159265359;
 const factor = 0.25 / 255.0;
 const c = 43758.5453;
@@ -14,24 +32,18 @@ fn randUV( uv: vec2<f32> ) -> f32{
     let sn = modf( dt / PI ).fract;
     return fract(sin(sn) * c);
 }
-struct VSOut {
-    @builtin(position) Position: vec4f,
-    @location(0) color: vec3f,
-};
-
-@vertex
-fn main(@location(0) inPos: vec3f,
-        @location(1) inColor: vec3f) -> VSOut {
-    var vsOut: VSOut;
-    vsOut.Position = vec4(inPos, 1.0);
-    vsOut.color = inColor;
-    return vsOut;
+const dither_fector = vec4<f32>( factor, -factor, factor, factor);
+fn dithering( color: vec4<f32>, puv: vec2<f32> ) -> vec4<f32> {
+    let grid_position = randUV( puv );
+    return color + mix( 2.0 * dither_fector, -2.0 * dither_fector, grid_position );
 }
-`;
-const fragWGSL = `
+
 @fragment
-fn main(@location(0) inColor: vec3f) -> @location(0) vec4f {
-    return vec4f(inColor, 1.0);
+fn main(
+	@location(0) inColor: vec3<f32>,
+	@location(1) pos: vec4<f32>
+	) -> @location(0) vec4<f32> {
+    return dithering(vec4<f32>(inColor, 1.0), pos.xy);
 }
 `;
 
